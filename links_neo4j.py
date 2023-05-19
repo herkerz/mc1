2ff95e1b6049a73ed6763ec3ec89281f5e6e54ca
Herman Kerz <108584839+herkerz@users.noreply.github.com>
2023-05-19 11:36:41 -0300

    Add files via upload

---
+++ links_neo4j.py
@@ -0,0 +1,68 @@
+from neo4j import GraphDatabase
+import csv
+
+# Neo4j connection details
+uri = "bolt://localhost:7687"
+username = "neo4j"
+password = "a"
+
+# CSV file path
+csv_file = "links.csv"
+
+# Cypher query to create relationships
+create_query = """
+LOAD CSV WITH HEADERS FROM 'file:///{csv_file}' AS row
+MATCH (source:Nodos), (target:Nodos)
+WHERE source.id = row.source AND target.id = row.target AND row.type = 'ownership'
+CREATE (source)-[:ownership]->(target)
+"""
+
+# Connect to Neo4j
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+# Open a Neo4j session
+with driver.session() as session:
+    # Execute the Cypher query to create relationships
+    session.run(create_query.format(csv_file=csv_file))
+
+# Cypher query to create relationships
+create_query = """
+LOAD CSV WITH HEADERS FROM 'file:///{csv_file}' AS row
+MATCH (source:Nodos), (target:Nodos)
+WHERE source.id = row.source AND target.id = row.target AND row.type = 'membership'
+CREATE (source)-[:membership]->(target)
+"""
+
+# Open a Neo4j session
+with driver.session() as session:
+    # Execute the Cypher query to create relationships
+    session.run(create_query.format(csv_file=csv_file))
+    
+# Cypher query to create relationships
+create_query = """
+LOAD CSV WITH HEADERS FROM 'file:///{csv_file}' AS row
+MATCH (source:Nodos), (target:Nodos)
+WHERE source.id = row.source AND target.id = row.target AND row.type = 'family_relationship'
+CREATE (source)-[:family_relationship]->(target)
+"""
+
+# Open a Neo4j session
+with driver.session() as session:
+    # Execute the Cypher query to create relationships
+    session.run(create_query.format(csv_file=csv_file))
+
+
+# Cypher query to create relationships
+create_query = """
+LOAD CSV WITH HEADERS FROM 'file:///{csv_file}' AS row
+MATCH (source:Nodos), (target:Nodos)
+WHERE source.id = row.source AND target.id = row.target AND row.type = 'partnership'
+CREATE (source)-[:partnership]->(target)
+"""
+
+# Open a Neo4j session
+with driver.session() as session:
+    # Execute the Cypher query to create relationships
+    session.run(create_query.format(csv_file=csv_file))
+# Close the Neo4j driver
+driver.close()
---
+++ nodes_neo4j.py
@@ -0,0 +1,30 @@
+from neo4j import GraphDatabase
+import csv
+
+# Neo4j connection details
+uri = "bolt://localhost:7687"
+username = "neo4j"
+password = "a"
+
+# CSV file path
+csv_file = "nodes.csv"
+
+# Cypher query to create nodes and relationships
+create_query = """
+LOAD CSV WITH HEADERS FROM 'file:///{csv_file}' AS row
+CREATE (n:Nodos)
+SET n.type = row.type,
+    n.country = row.country,
+    n.id = row.id
+"""
+
+# Connect to Neo4j
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+# Open a Neo4j session
+with driver.session() as session:
+    # Execute the Cypher query to create nodes and relationships
+    session.run(create_query.format(csv_file=csv_file))
+
+# Close the Neo4j driver
+driver.close()
